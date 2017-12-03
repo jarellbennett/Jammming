4a47b7f022169fe8207e8c8a2c1cd7d09c2ff6e4
Jarell <jb123506@gmail.com>
2017-12-03 18:26:41 -0500

    Added .map() method

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,7 +5,11 @@ class TrackList extends React.Component {
   render(){
     return(
       <div className="TrackList">
-          //<!-- You will add a map method that renders a set of Track components  -->
+        {
+          this.props.tracks.map(track => {
+            return <Track track={this.props.track} key={track.id} />;
+        });
+      }
       </div>
     );
   }
