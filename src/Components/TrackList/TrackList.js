6d16b6e881fa43a521c0199daf5e45ee93d58aa2
Jarell <jb123506@gmail.com>
2017-12-04 22:27:33 -0500

    onAdd property

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -8,7 +8,8 @@ class TrackList extends React.Component {
       <div className="TrackList">
         {
           this.props.tracks.map(track => {
-            return <Track track={this.props.track} key={track.id} />;
+            return <Track track={this.props.track} key={track.id}
+              onAdd={this.props.onAdd} />;
         })
       }
       </div>
