3df6bece07423a584ab3516fdad5dea4a12c5101
Jarell <jb123506@gmail.com>
2017-12-11 00:59:31 -0500

    import Spotify, search method in spotify

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -8,11 +8,33 @@ const Spotify = {
         return accessToken;
       }
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-        const expireIn = window.location.href.match(/expires_in=([^&]*)/);
-     if(accessTokenMatch&&expireIn){
+        const expire = window.location.href.match(/expires_in=([^&]*)/);
+     if(accessTokenMatch&&expire){
+       accessToken = accessTokenMatch[1];
+       const expiresIn = Number(expire[1]);
        window.setTimeout(() => accessToken = '', expiresIn * 1000);
        window.history.pushState('Access Token', null, '/');
-        }
-  }
+     } else {
+        window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
+     }
+  },
+
+  search(term){
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+    { headers: {Authorization: `Bearer ${accessToken}`}
+  }).then(response => response.json()).then(jsonResponse => {
+    if(jsonResponse.tracks){
+      return jsonResponse.tracks.items.map(track => {
+        return {
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri }
+      });
+    } else {
+      return [];}
+  })
+}
 }
 export default Spotify;
