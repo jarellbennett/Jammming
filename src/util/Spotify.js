d9e00911535d4af4bb56b27170d0ac7ec0d85ddd
Jarell <jb123506@gmail.com>
2017-12-15 17:39:20 -0500

    savePlaylist() incomplete

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -31,11 +31,13 @@ const Spotify = {
           name: track.name,
           artist: track.artists[0].name,
           album: track.album.name,
-          uri: track.uri 
+          uri: track.uri
       }));
     } else {
       return [];}
-  });
- }
+   });
+  },
+
+  
 }
 export default Spotify;
