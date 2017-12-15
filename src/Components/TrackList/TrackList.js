241e4cb2d41fd4712460c14296fdbdb092f9ee4c
Jarell <jb123506@gmail.com>
2017-12-15 17:32:44 -0500

    Changed property in track component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -8,7 +8,7 @@ class TrackList extends React.Component {
       <div className="TrackList">
         {
           this.props.tracks.map(track => {
-            return <Track track={this.props.track} key={track.id}
+            return <Track track={track} key={track.id}
               onAdd={this.props.onAdd} isRemoval={this.props.isRemoval}
               onRemove={this.props.onRemove}/>;
         })
